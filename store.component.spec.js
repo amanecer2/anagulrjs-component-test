aa72dd862cfc28ac9971be3f4076fa14aab8bd4c
amanecer <sh2le86@gmail.com>
2017-08-23 14:45:03 +0300

    Update store.component.spec.js

--- store.component.spec.js
+++ store.component.spec.js
@@ -2,23 +2,28 @@ describe('store component', function() {
   
   const sevenEleven = '7-eleven';
   let store;
+  /* mock the dependencies for this component*/
   let mockConfig = {store: "https://s3-us-west-1.amazonaws.com/wizdraw-test/store",};
   let $componentController;
   let component;
 
+  /*$componentController create the instance of component*/
   function createComponent(bindings) {
     ///$componentController('compoenent name',{ decencies injection}, bindings)
     return $componentController('wdStore', {configApi:mockConfig}, bindings);
   }
   
+  /*get the angular module we want to test*/
   beforeEach(module( 'wdStore' ));
 
+  /*inject for every test the component*/
   beforeEach(inject(function (_$componentController_) {
     $componentController = _$componentController_;
   }));
 
   describe('$onInit ',()=>{
 
+    /* for eveery test make the on init life sycle*/
     beforeEach(()=>{
       component = createComponent({store: sevenEleven, prop2: 0});
       component.$onInit();
