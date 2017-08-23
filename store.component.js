c6ddbb6cec507dbe569be1034379ca53fc46fed6
amanecer <sh2le86@gmail.com>
2017-08-23 14:36:16 +0300

    Create store.component.js

---
+++ store.component.js
@@ -0,0 +1,50 @@
+/**
+ * Wic suppliers store
+ * @description show wic store that user can pay to them
+ * @example <wd-store store="store" on-update="$ctrl.setPaymentAgency(data)" ></wd-store>
+ * @store => 7-eleven/circle-k
+ *
+ */
+(function () {
+  'use strict';
+
+  class StoreComponent {
+
+    constructor(configApi) {
+      this.configApi = configApi;
+    }
+
+    $onInit() {
+      this.name = this.store.replace('-', ' ').toLowerCase();
+      this.src  = `${this.configApi.store}/${this.store.replace('-', '').toLowerCase()}.png`;
+      this.data = this.store.toLowerCase();
+    }
+
+    $onChanges(change){}
+
+    onClick(name){
+      this.onUpdate({data: this.data});
+    }
+  }
+
+  angular.module('wdStore', [])
+    .component('wdStore', {
+      template: `
+      <div class="item list-width-90"  ng-click="$ctrl.onClick($ctrl.src)">
+            <div class="col-30">
+                <img  ng-src="{{$ctrl.src}}">
+            </div>
+          <div class="col-70 agency">
+              {{$ctrl.name}}
+          </div>
+            <i class="ion-chevron-right color-blue-main"></i>
+        </div>
+      `,
+      controller: ['configApi', StoreComponent],
+      bindings: {
+        store: '<',
+        onUpdate: '&'
+      }
+    });
+
+}());
